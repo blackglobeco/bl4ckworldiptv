a4893b763c9074898a304f58704ee380a5c5cb72
Black Globe <137195478+blackglobeco@users.noreply.github.com>
2026-06-14 18:28:13 +0800

    Update Settings.js

--- src/Settings.js
+++ src/Settings.js
@@ -210,7 +210,7 @@ const BUILT_IN_PLAYLISTS = [
   { name: "🇪🇭 Western Sahara", url: "https://iptv-org.github.io/iptv/countries/eh.m3u" },
   { name: "🇾🇪 Yemen", url: "https://iptv-org.github.io/iptv/countries/ye.m3u" },
   { name: "🇿🇼 Zimbabwe", url: "https://iptv-org.github.io/iptv/countries/zw.m3u" },
-  { name: "📡 Astro", url: "https://live.hacks.tools/iptv/languages/malaysia.m3u" },
+  { name: "📡 Astro", url: "https://github.com/blackglobeco/bl4ckworldiptv/delete/main/astro_iptv.m3u" },
 ];
 
 export default function Settings() {
